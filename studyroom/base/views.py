b1ece273b7c578cc6818d86f914e670bb5b2241d
Piyush <impiyush016@gmail.com>
2022-05-26 11:44:34 +0530

    Database and admin panel

--- studyroom/base/views.py
+++ studyroom/base/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "base/home.html")
+    rooms = Room.objects.all()
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
+
+
+def room(request, pk):
+    room = Room.objects.get(id=int(pk))
+    context = {"room": room}
+    return render(request, "base/room.html", context)

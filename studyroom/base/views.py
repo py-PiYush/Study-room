a1b2dec07d6951b22d59aa9e5b55b40c53ad75cd
Piyush <impiyush016@gmail.com>
2022-05-27 11:42:52 +0530

    User profile

--- studyroom/base/views.py
+++ studyroom/base/views.py
@@ -62,7 +62,13 @@ def home(request):
     rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q))
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    context = {
+        "rooms": rooms,
+        "topics": topics,
+        "room_count": room_count,
+        "room_messages": room_messages,
+    }
     return render(request, "base/home.html", context)
 
 
@@ -84,6 +90,20 @@ def room(request, pk):
         "participants": participants,
     }
     return render(request, "base/room.html", context)
+
+
+def user_profile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {
+        "user": user,
+        "rooms": rooms,
+        "room_messages": room_messages,
+        "topics": topics,
+    }
+    return render(request, "base/profile.html", context=context)
 
 
 @login_required(login_url="login")
